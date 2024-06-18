c6f783c9100c086eee66cc73ea2ce76db7fe39f6
DrPerkyLegit <116128211+DrPerkyLegit@users.noreply.github.com>
2024-06-18 11:54:10 -0400

    testing for ingame

--- src/Board.lua
+++ src/Board.lua
@@ -24,7 +24,7 @@ end
 board.run = function (arguments)
     LoadConfig()
 
-    local finalMessage = Config.message
+    local finalMessage = Config.message.text
 
     while #finalMessage < displayWidth do
         finalMessage = finalMessage .. " "
@@ -36,8 +36,12 @@ board.run = function (arguments)
         for i = 1, textLength do
             screen.clear()
 
-            --screen.setCursorPos(displayWidth - )
-
+            screen.setTextColor(utils.hexToColor(Config.title.color))
+            screen.setBackgroundColor(utils.hexToColor(Config.backgroundColor))
+            screen.setCursorPos((displayWidth / 2) - (#Config.message.title / 2), displayHeight / 1.5)
+            screen.write(Config.title.text)
+            
+            screen.setTextColor(utils.hexToColor(Config.message.color))
             screen.setCursorPos(1, displayHeight / 2)
             local displayText = finalMessage:sub(i, textLength) .. finalMessage:sub(1, i - 1)
             screen.write(displayText:sub(1, displayWidth))
